dc26dfd970b1b844a6877dcc296eaeb2095e2a5d
Yulia <yliahavrylyak@gmail.com>
2021-02-02 17:48:23 +0200

    Add HW_1

---
+++ HW_1/1.py
@@ -0,0 +1,20 @@
+print("What is your name?")
+user_name = input("Hello, ")
+print(user_name, type(user_name))
+
+print("How old are you?")
+user_age = input("Your age is ")
+print(user_age, type(user_age))
+
+print("Where do you live? ")
+user_city = input("You live in ")
+print(user_city, type(user_city))
+
+a = 2
+b = 2
+print("a + b : ", a + b)
+print("a - b : ", a - b)
+print("a * b : ", a * b)
+print("a / b : ", a / b)
+print("a ** b : ", a ** b)
+

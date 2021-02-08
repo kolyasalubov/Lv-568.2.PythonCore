f33b1623d65dda8655981eb0a4bf6c01e3aa4786
Bohdana Dmytruk <dmytruk.b922002@gmail.com>
2021-02-08 23:42:45 +0200

    added HW_1

---
+++ HW_1/Bohdana_Dmytruk/Home_Work1_Task_2.py
@@ -0,0 +1,12 @@
+a = int(input("write number for first var "))
+b = int(input("write number for second var "))
+c = a+b
+print(c)
+c = a-b
+print(c)
+c = a/b
+print(c)
+c = a*b
+print(c)
+c = a**b
+print(c)

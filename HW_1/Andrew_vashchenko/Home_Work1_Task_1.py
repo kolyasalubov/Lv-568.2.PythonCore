d73f40c96668b7582f2cf7955667a4a9e5b76abd
Andrew Vashchenko <andrew_vashchenko@icloud.com>
2021-02-09 21:42:06 +0200

    Added HW_1

---
+++ HW_1/Andrew_vashchenko/Home_Work1_Task_1.py
@@ -0,0 +1,7 @@
+name = input('What is your name? ')
+age = int(input('How old are you? '))
+city = input('Where do you live? ')
+
+print("Nice to meet you,", name, end="! ")
+print("Your age is", age)
+print('You live in', city, end='.')

5cef822da3e3f0ca35e37ddcebbe0233e9f343b9
VRB-Python <v.bodnar2013@gmail.com>
2021-02-09 16:20:06 +0200

    Added Bodnar folder to HW_4

--- HW_4/Home_Work4_Task_1.py
+++
@@ -1,7 +0,0 @@
-x = int(input())
-x = list(range(x+1))
-x.pop(0)
-y = 1
-for i in x:
-     y *= i
-print(f"х! = {y}")
--- HW_4/Home_Work4_Task_3.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-fib = [0,1,1]
-while n not in range(fib[-1]+1):
-    fib.append(fib[-2]+fib[-1])
-print(fib)

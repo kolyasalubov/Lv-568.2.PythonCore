f9b075b6720606e734cf2fbe62ba2618c94fb30b
Yulia <yliahavrylyak@gmail.com>
2021-02-11 15:43:24 +0200

    Added HW5

---
+++ HW_5/Home_Work5_Task_1.py
@@ -0,0 +1,9 @@
+for i in range(1,10):
+     if i % 2 == 0:
+         print("Nums is divisible by 2 : ", i)
+for i in range(1,10):
+     if i % 3 == 0:
+         print("Nums is divisible by 3 : ", i)
+for i in range(1,10):
+     if i % 2 !=0 and i % 3 !=0:
+         print("Nums is not divisible by 2,3 : ", i)
---
+++ HW_5/Home_Work5_Task_2.py
@@ -0,0 +1,6 @@
+log_user = input("Type your login: ")
+while log_user == "First":
+     print("Hello")
+     break
+else:
+    print("Error")

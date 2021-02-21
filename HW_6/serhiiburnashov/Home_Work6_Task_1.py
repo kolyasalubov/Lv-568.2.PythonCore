b67de1ec8eb068ce8adc6b151fa3aa9938031207
“Serhii <burnashowserega@mail.com>
2021-02-21 21:05:28 +0200

    Add HW_6 change

--- HW_6/serhiiburnashov/Home_Work6_Task_1.py
+++ HW_6/serhiiburnashov/Home_Work6_Task_1.py
@@ -1,14 +1,14 @@
-def  largest_number_of_two_numbers(first_number, second_number):
+def largest_number_of_two_numbers(first, second):
     """This function returns the largest number of two numbers."""
 
-    largest_number = max(first_number, second_number)
-    return largest_number
+    largest_num = max(first, second)
+    return largest_num
 
 
 first_number, second_number = (
-    int(input("Enter first number: ")), 
+    int(input("Enter first number: ")),
     int(input("Enter second number: "))
-    )
+)
 largest_number = largest_number_of_two_numbers(first_number, second_number)
 print(f"The largest number is: {largest_number}")
 print(largest_number_of_two_numbers.__doc__)
--- HW_6/serhiiburnashov/simple-find-the-distance-between-two-points.py
+++ HW_6/serhiiburnashov/simple-find-the-distance-between-two-points.py
@@ -1,7 +1,7 @@
 def distance(x1, y1, x2, y2):
     """Given two ordered pairs calculate the distance between them."""
 
-    distance_between_two_points = (((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1))) ** 0.5
+    distance_between_two_points = (((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))) ** 0.5
     return round(distance_between_two_points, 2)
 
 

01d91d84d27ef7adf145da52f3313e8b08fa1063
Bohdana Dmytruk <dmytruk.b922002@gmail.com>
2021-03-01 22:56:08 +0200

    HW_9

---
+++ HW_9/Bohdana_Dmytruk/Kata1_regular-ball-super-ball.py
@@ -0,0 +1,5 @@
+class Ball(object):
+    # your code goes here
+    def __init__(self, ball_type="regular"):
+        self.ball_type = ball_type
+
---
+++ HW_9/Bohdana_Dmytruk/Kata3_basic-subclasses-adam-and-eve.py
@@ -0,0 +1,15 @@
+class Human():
+        pass
+    
+class Man(Human):
+        pass
+        
+class Woman(Human):
+        pass
+    
+def God():
+    hum1=Man()
+    hum2=Woman()
+    creatures=[hum1, hum2]
+
+    return creatures

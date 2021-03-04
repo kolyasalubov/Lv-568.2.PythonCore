a8efdfaa7a79c34f17e578b826be5b46672f480a
Andriana Khariv <andriana.lozinska@gmail.com>
2021-03-04 12:56:13 +0200

    add HW 6

---
+++ HW_6/Andriana_Khariv/Home_Work6_Task_2.py
@@ -0,0 +1,6 @@
+user_pass = input("enter password:")
+while user_pass == 'First':
+    print("password correct")
+    break
+else:
+    print("password incorrect")
---
+++ HW_6/Andriana_Khariv/Kata_6_2.py
@@ -0,0 +1,5 @@
+def filter_words(st):
+    st1 = st.lower()
+    st2 = st1.capitalize()
+    st3 = st2.expandtabs([9])
+    return st3

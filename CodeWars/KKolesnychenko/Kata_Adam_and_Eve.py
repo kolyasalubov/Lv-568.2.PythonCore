88f1255dbacba29e4cc490efcc5278fcb4ed7862
Kate Kolesnychenko <kkolesnychenk00@gmail.com>
2021-03-07 21:17:20 +0200

    Add HW8&Kata8

---
+++ CodeWars/KKolesnychenko/Kata_Adam_and_Eve.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self):
+        pass
+class Man(Human):
+    def __init__(self):
+        pass
+class Woman(Human):
+    def __init__(self):
+        pass
+
+
+def God():
+    return Man(),Woman()
